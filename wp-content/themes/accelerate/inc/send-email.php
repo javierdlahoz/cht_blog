942c0089a4f71c6f963fff448f51acc4f6b8330a
cadavidalvarado <carlosalvaradofarfan@gmail.com>
2018-08-05 21:39:19 -0500

    changue text button and add information message

--- wp-content/themes/accelerate/inc/send-email.php
+++ wp-content/themes/accelerate/inc/send-email.php
@@ -1,27 +1,26 @@
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <div class="modal fade" id="emailModalForm" role="dialog" displayed="false" data-backdrop="static" tabindex="-1">
   <div class="modal-dialog" role="document">
-    <div class="modal-email-content" >
+    <div class="modal-email-content">
       <div class="modal-email-header">
         <button type="button" class="modal-email-close" data-dismiss="modal" aria-label="Close" id="dismiss-email-modal">
           <i class="fa fa-close"></i>
-        </button>        
-        <h3 class="modal-title text-center">Let's Keep in Touch!</h3>
-        <h6 class="modal-title text-center">Get valuable information from the leading Nonprofit Topic Experts straight to your inbox. Subscribe to the CharityHowTo Blog</h6>
+        </button>
+        <h3 class="modal-email-title text-center">Let's Keep in Touch!</h3>
+        <h6 class="modal-email-title text-center">Get valuable information from the leading Nonprofit Topic Experts straight to your inbox.<br/> Subscribe to the CharityHowTo Blog.</h6>
       </div>
       <div class="modal-body">
+      <h6 class="modal-email-text text-center">Thank you for subscribing! :)</h6>
+      <h6 class="modal-email-text text-center">The subscription was not successful :(</h6>
       <form id="subscribe-email-form">
           <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="johndoe@mail.com" aria-label="johndoe@mail.com" aria-describedby="basic-addon2" id="subscriber-email">
-            <div class="input-group-append">
-              <button class="g-recaptcha" data-sitekey="6LfDCmgUAAAAAIN-vsdjtXEV2dyibM_5nYIDQX7H"
-                      data-callback='submitEmail' id="buttonemail" class="btn btn-success" type="button">SUBMIT</button>
-            </div>
           </div>
       </form>
       </div>
       <div class="modal-email-footer">
-      <p class="text-center">I Want to Subscribe!</p>
+      <button class="g-recaptcha" data-sitekey="6LfDCmgUAAAAAIN-vsdjtXEV2dyibM_5nYIDQX7H"
+                      data-callback='submitEmail' id="buttonemail" class="btn btn-success" type="button">I WANT TO SUBSCRIBE!</button>
       </div>
     </div>
   </div>
